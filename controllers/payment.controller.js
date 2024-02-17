0993b4fb854d08a622d9000ff0e24b55e90c63a0
news_aggregator_api <aanand.py@gmail.com>
2024-02-18 01:39:55 +0530

    created controller for Payment

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -4,7 +4,25 @@ const constants = require('../utils/constants')
 
 
 exports.getAllPayments = async (req,res)=>{
+    try{
+        const queryObj = {};
+        if(user.userType === constants.userTypes.admin){
 
+        }else{
+            const booking = await Booking.findOne({
+                _id : req.userId
+            })
+            const bookingIds = booking.map(b=>b._id);
+            queryObj._id = { $in : bookingIds}
+        }
+        const payments = await Payment.find()
+        res.status(200).send(payments)
+    }catch (err) {
+        res.status(500).send({
+            message: "Some Internal Error Occured",
+            err : err.message
+        })
+    } 
 }
 
 exports.getPaymentOnId = async (req,res)=>{
@@ -22,5 +40,35 @@ exports.getPaymentOnId = async (req,res)=>{
 }
 
 exports.createPayment = async(req,res)=>{
+    const booking = await Booking.findOne({
+        _id : req.body.bookingId
+    })
 
+    var paymentObj = {
+        bookingId : req.body.bookingId,
+        amount : req.body.amount,
+        status : constants.paymentStatus.success
+    }
+    var bookingTime = booking.createdAt;
+    var currentTime = Date.now();
+
+    var minutes = Math.floor(((currentTime - bookingTime) / 1000) / 60);
+    if(minutes > 5) {
+        booking.status = constants.bookingStatus.expired;
+        await booking.save();
+        return res.status(200).send({
+            message: "Can't do the payment as the booking is delayed and expired"
+        })
+    }
+    try{
+        const payment = await Payment.create(paymentObj)
+        booking.status = constants.bookingStatus.completed
+        await booking.save();
+        return res.status(200).send(payment)
+    }catch (err) {
+        res.status(500).send({
+            message: "Some Internal Error Occured",
+            err : err.message
+        })
+    } 
 }
